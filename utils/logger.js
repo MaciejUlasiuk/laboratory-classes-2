fcd2b93859520a285a2a0821a1212d15b4075e41
MaciejUlasiuk <filip123xd@o2.pl>
2025-04-02 05:23:47 +0200

    router&loggerchanges

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,27 @@
 // 🔄 Refactoro the Changer  
 // Przenieś teraz odpowiednie wywołania logów z routing.js i zastąp tam logowanie bezpośrednie wywołaniem tych funkcji.
 
-const getInfoLog = () => {};
+const getInfoLog = (url, method) => {
+  console.log(
+    `INFO (${new Date(Date.now()).toUTCString()}): ${method} - ${url}`
+  );
+};
 
-const getErrorLog = () => {};
+const getErrorLog = (url) => {
+  console.warn(
+    `ERROR (${new Date(
+      Date.now()
+    ).toUTCString()}): requested url ${url} doesn't exist.`
+  );
+};
 
-const getProcessLog = () => {};
+const getProcessLog = (log) => {
+  console.log(
+    `PROCESS (${new Date(
+      Date.now()
+    ).toUTCString()}): ${log}`
+  );
+};
 
 module.exports = {
   getInfoLog,
